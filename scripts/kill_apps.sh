7c9c723d845a301854704f69194ece2c475908ff
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-03-21 17:32:51 +0100

    more defensive about fialing script

--- scripts/kill_apps.sh
+++ scripts/kill_apps.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Killing all apps"
-pkill -f eureka || echo "Failed to kill any apps"
+pkill -f eureka-dalston || echo "Failed to kill any dalston apps"
+pkill -f eureka-edgware || echo "Failed to kill any edgware apps"
+pkill -f eureka-finchley || echo "Failed to kill any finchley apps"
