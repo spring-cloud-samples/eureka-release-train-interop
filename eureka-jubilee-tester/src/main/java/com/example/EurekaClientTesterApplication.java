43531fe59c230cf1bd2b6fd05de19ebb9074c93d
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2021-08-20 11:50:28 +0200

    fixed compilation

--- eureka-jubilee-tester/src/main/java/com/example/EurekaClientTesterApplication.java
+++ eureka-jubilee-tester/src/main/java/com/example/EurekaClientTesterApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
